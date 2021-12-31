c4eaa4e655f2b343ebd3bd8ddedb57c94b8a6031
hanalei <82703789+pg-hanalei@users.noreply.github.com>
2021-12-31 19:09:01 +0900

    update Hello!

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout title="Home">
-      <p className="text-4xl">Hello Next js</p>
+      <p className="text-4xl">Hello!! Next js</p>
     </Layout>
   );
 }
